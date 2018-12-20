69fef789fe56675937c01d44d3e82470aba5a8c7
Ahmed Ali <ahmed@admica>
2018-12-20 23:51:45 +0200

    readme and error handeling

--- src/utils/FSAPI.js
+++ src/utils/FSAPI.js
@@ -9,22 +9,49 @@ const NUMBER_OF_RESULTS = 25
 
 export const getRestaurants = () =>
     fetch(`${URL}/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}&limit=${NUMBER_OF_RESULTS}&ll=52.5200,13.4050&query=coffee&radius=${RADIUS_N_METERS}`)
-        .then(res => res.json())
+        .then(res => {
+          if (res.status >= 400 && res.status < 600) {
+            alert('Error Connecting to the FourSquare Api');
+            throw new Error("Bad response from server");
+            
+          }
+          return res.json()
+        })
+        
+        
         .then(data => data)
+        
 
 export const getRestaurantDetails = (venueID) =>
     fetch(`${URL}/${venueID}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}`)
-        .then(response => response.json()
+        .then(response => {
+          if (response.status >= 400 && response.status < 600) {
+            alert('Error Connecting to the FourSquare Api');
+            throw new Error("Bad response from server");
+            
+          }
+          return response.json()
           .then(text => ({
             json: text,
             meta: response
           }))
+        }
+          
         )
         .then(({ json, meta }) => {
           return json
         })
+        
 
 export const search = () =>
     fetch(`${URL}/search?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}&ll=39.0997265,-94.57856670000001&query=barbecue&radius=${RADIUS_N_METERS}&intent=browse&limit=20`)
-        .then(response => response.json())
+        .then(response =>{
+          if(response.status >= 400 && response.status < 600){
+            alert('Error Connecting to the FourSquare Api');
+            throw new Error("Bad response from server");
+          }
+          return response.json()
+        })
+        
         .then(response => console.log(response))
+        
